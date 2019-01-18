9bfbbb9744240830f1400043416b247dd67afbd4
Karsten Bock <KarstenBock@gmx.net>
2019-01-18 14:47:24 +0100

    - Started working on the component system.

---
+++ game_object.gd
@@ -0,0 +1,25 @@
+extends Node
+
+func _ready():
+    pass
+
+func has_component(name):
+    return has_node("Components/" + name)
+    
+func get_component(name):
+    if !has_component(name): # Check exists to keep the log error free.
+        return null
+    return get_node("Components/" + name)
+    
+func create_and_add_component(component_class, name):
+    if has_component(name):
+        return null
+    var components = get_node("Components")
+    var new_component = component_class.new()
+    new_component.name = name
+    components.add_child(new_component)
+    return new_component
+    
+func _get(property):
+    if (has_component(property)):
+        return get_component(property)
---
+++ test/unit/test_game_object.gd
@@ -0,0 +1,50 @@
+extends "res://addons/gut/test.gd"
+
+const GameObject = preload("res://game_object.gd")
+
+var node;
+var components;
+
+func before_each():
+    node = Node.new()
+    components = Node.new()
+    components.name = "Components"
+    node.add_child(components)
+    node.set_script(GameObject)  
+    
+func test_has_returns_true_on_existing_component():
+    var test_component = Node.new()
+    test_component.name = "Test"
+    components.add_child(test_component)
+    assert_true(node.has_component("Test"), "Expected has_component to return true")
+
+func test_has_returns_false_for_non_existing_component():
+    assert_false(node.has_component("Test"), "Expected has_component to return false")
+
+func test_get_returns_existing_component():
+    var test_component = Node.new()
+    test_component.name = "Test"
+    components.add_child(test_component)
+    assert_eq(node.get_component("Test"), test_component, "Expected get_component to return the correct component")
+
+func test_get_returns_null_for_non_existing_component():
+    assert_eq(node.get_component("Test"), null, "Expected get_component to return null")
+    
+func test_create_and_add_adds_non_existing_component():
+    var component = node.create_and_add_component(Node, "Test")
+    assert_ne(component, null, "Expected non null return value")
+    assert_true(node.is_a_parent_of(component), "Expected value to be a child of Components")
+
+func test_create_and_add_does_not_add_if_component_already_exists():
+    var test_component = Node.new()
+    test_component.name = "Test"
+    components.add_child(test_component)
+    var component = node.create_and_add_component(Node, "Test")
+    assert_eq(component, null, "Expected null return value")
+    assert_true(node.is_a_parent_of(test_component), "Expected previous component to still be a child of Components")
+
+func test_get_operator_returns_existing_component():
+    var test_component = Node.new()
+    test_component.name = "Test"
+    components.add_child(test_component)
+    assert_eq(node.Test, test_component, "Expected GameObject.Test to return the correct component")
