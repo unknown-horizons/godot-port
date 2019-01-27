e191bb2f15138e6daaa5b558239b00588d229591
Karsten Bock <KarstenBock@gmx.net>
2019-01-27 10:43:44 +0100

    Add base class for components

---
+++ Assets/World/component.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var modified = false
+
+func _ready():
+    pass
+
+func get_serializable_fields():
+    return _get_serializable_fields()
+
+func _get_serializable_fields():
+    pass
