737c1ba4d2250d94bbe29633022f2f108d290f6c
Karsten Bock <KarstenBock@gmx.net>
2019-01-29 07:25:28 +0100

    Movement now also handles spatial nodes

--- Assets/World/systems/movement.gd
+++ Assets/World/systems/movement.gd
@@ -5,10 +5,22 @@ const GameObject = preload("res://Assets/World/gameObject.gd")
 func _process(delta):
     for game_object in world.get_objects_with_component("position"):
         var position = game_object.get_component("position")
+        var node = position.node
+        if node == null:
+            return
         if position.modified:
-            position.node.position = position.vector
-            position.node.rotation_degrees = position.rotation
+            if node is Node2D:
+                node.position = position.vector
+                node.rotation_degrees = position.rotation
+            elif node is Spatial:
+                node.translation.x = position.vector.x
+                node.translation.y = position.vector.y
+                node.rotation_degrees.y = position.rotation
             position.modified = false
         else:
-            position.vector = position.node.position
-            position.rotation = position.node.rotation_degrees
+            if node is Node2D:
+                position.vector = node.position
+                position.rotation = node.rotation_degrees
+            elif node is Spatial:
+                position.vector = Vector2(node.translation.x, node.translation.y)
+                position.rotation = node.rotation_degrees.y
--- test/unit/test_movement_system.gd
+++ test/unit/test_movement_system.gd
@@ -9,31 +9,32 @@ var world
 var map
 var position
 var movement_system
-var node
+var test_object
+
 
 func before_each():
     map = Node.new()
     world = GameWorld.new()
     map.add_child(world)
-    var test_object = GameObject.new()
+    test_object = GameObject.new()
     var components = Node.new()
     components.name = "Components"
     test_object.add_child(components)
     map.add_child(test_object)
     position = test_object.create_and_add_component(PositionComponent, "position")
-    node = Node2D.new()
-    test_object.add_child(node)
+    movement_system = MovementSytem.new()
+    world.add_child(movement_system)
+    
+func test_process_sets_node2d_values_when_component_modified():
+    gut.p("Test started")
+    var node = Node2D.new()
+    map.add_child(node)
     node.position.x = 0
     node.position.y = 0    
     node.rotation_degrees = 0
     position.node = node
     position.vector = node.position
     position.rotation = node.rotation_degrees
-    movement_system = MovementSytem.new()
-    world.add_child(movement_system)
-    
-func test_process_sets_node_values_when_component_modified():
-    gut.p("Test started")
     var test_x = 100.0
     var test_angle = 45.0
     position.vector.x = test_x
@@ -45,8 +46,16 @@ func test_process_sets_node_values_when_component_modified():
     assert_false(position.modified, "Expected modified to have been reset")
     gut.p("Test ended")  
     
-func test_process_sets_component_values_to_node_if_component_not_modified():
+func test_process_sets_component_values_to_node2d_if_component_not_modified():
     gut.p("Test started")
+    var node = Node2D.new()
+    map.add_child(node)
+    node.position.x = 0
+    node.position.y = 0    
+    node.rotation_degrees = 0
+    position.node = node
+    position.vector = node.position
+    position.rotation = node.rotation_degrees
     var test_y = 150.0
     var test_angle = 225.0
     node.position.y = test_y
@@ -56,3 +65,45 @@ func test_process_sets_component_values_to_node_if_component_not_modified():
     assert_eq(position.rotation, test_angle, "Expected rotation to have been updated")
     gut.p("Test ended")
     
+func test_process_sets_spatial_values_when_component_modified():
+    gut.p("Test started")
+    var spatial = Spatial.new()
+    map.add_child(spatial)
+    spatial.translation.x = 0.0
+    spatial.translation.y = 0.0
+    spatial.translation.z = 0.0
+    position.node = spatial
+    position.vector = Vector2(spatial.translation.x, spatial.translation.y)
+    position.rotation = spatial.rotation_degrees.y
+    var test_x = 100.0
+    var test_y = 150.0
+    var test_angle = 45.0
+    position.vector.x = test_x
+    position.vector.y = test_y
+    position.rotation = test_angle
+    position.modified = true
+    gut.simulate(map, 1, .1)
+    assert_eq(spatial.translation.x, test_x, "Expected position to have been updated")
+    assert_eq(spatial.translation.y, test_y, "Expected position to have been updated")
+    assert_eq(spatial.rotation_degrees.y, test_angle, "Expected rotation to have been updated")
+    assert_false(position.modified, "Expected modified to have been reset")
+    gut.p("Test ended")
+
+func test_process_sets_component_values_to_spatial_if_component_not_modified():
+    gut.p("Test started")
+    var spatial = Spatial.new()
+    map.add_child(spatial)
+    position.node = spatial
+    position.vector = Vector2(spatial.translation.x, spatial.translation.y)
+    position.rotation = spatial.rotation_degrees.y
+    var test_x = 100.0
+    var test_y = 150.0
+    var test_angle = 45.0
+    spatial.translation.x = test_x
+    spatial.translation.y = test_y
+    spatial.rotation_degrees.y = test_angle
+    gut.simulate(map, 1, .1)
+    assert_eq(position.vector.x, test_x, "Expected position to have been updated")
+    assert_eq(position.vector.y, test_y, "Expected position to have been updated")
+    assert_eq(position.rotation, test_angle, "Expected rotation to have been updated")
+    gut.p("Test ended")    
