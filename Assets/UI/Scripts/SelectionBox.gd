53692f37fe6e34723ded15743f06ff1251d55d9e
L <artism90@googlemail.com>
2019-09-03 21:51:25 +0200

    Set up pathfinding

---
+++ Assets/UI/Scripts/SelectionBox.gd
@@ -0,0 +1,18 @@
+extends Control
+
+var is_visible = false
+var m_pos = Vector2()
+var start_sel_pos = Vector2() # first click position
+
+const sel_box_col = Color(1, 1, 1)
+const sel_box_line_width = 1
+
+func _draw() -> void:
+	if is_visible and start_sel_pos != m_pos:
+		draw_line(start_sel_pos, Vector2(m_pos.x, start_sel_pos.y), sel_box_col, sel_box_line_width) # upper right
+		draw_line(start_sel_pos, Vector2(start_sel_pos.x, m_pos.y), sel_box_col, sel_box_line_width) # lower left
+		draw_line(m_pos, Vector2(m_pos.x, start_sel_pos.y), sel_box_col, sel_box_line_width) # upper left
+		draw_line(m_pos, Vector2(start_sel_pos.x, m_pos.y), sel_box_col, sel_box_line_width) # lower right
+
+func _process(delta: float) -> void:
+	update()
---
+++ Assets/World/AStarMap.gd
@@ -0,0 +1,80 @@
+extends Spatial
+
+var all_points = {}
+var as_node: AStar = null
+
+onready var grid_map = $GridMap
+
+var ray_plane := MeshInstance.new() as MeshInstance
+
+func _ready() -> void:
+	ray_plane.mesh = PlaneMesh.new()
+	ray_plane.visible = false
+
+	ray_plane.scale = Vector3(128, 128, 128) # TODO: Adapt size dynamically to GridMap's outer bounds
+
+	#ray_plane.create_convex_collision() # crashes for GLES2 in the export build
+#	var static_body = StaticBody.new()
+#	var collision_shape = CollisionShape.new()
+#	collision_shape.shape = PlaneShape.new()
+
+	ray_plane.set("material/0", SpatialMaterial.new())
+	ray_plane.get("material/0").set("flags_no_depth_test", true)
+	ray_plane.get("material/0").set("albedo_color", Color("1863d3"))
+	add_child(ray_plane)
+	
+	as_node = AStar.new()
+	var cells = grid_map.get_used_cells()
+	for cell in cells:
+		if _get_cell_item_name(cell) in [
+										"deep",
+										"shallow_deep",
+										"shallow_curve_in",
+										"shallow_deep_curve_out"
+										]:
+			var index = as_node.get_available_point_id()
+			as_node.add_point(index, grid_map.map_to_world(cell.x, cell.y, cell.z))
+			all_points[v3_to_index(cell)] = index
+	
+	for cell in cells:
+		if _get_cell_item_name(cell) in [
+										"deep",
+										"shallow_deep",
+										"shallow_curve_in",
+										"shallow_deep_curve_out"
+										]:
+			for x in [-1, 0, 1]:
+				for y in [-1, 0, 1]:
+					for z in [-1, 0, 1]:
+						var v3 = Vector3(x, y, z)
+						if v3 == Vector3(0, 0, 0):
+							continue
+						if v3_to_index(v3 + cell) in all_points:
+							var ind1 = all_points[v3_to_index(cell)]
+							var ind2 = all_points[v3_to_index(cell + v3)]
+							if !as_node.are_points_connected(ind1, ind2):
+								as_node.connect_points(ind1, ind2, true)
+
+func _get_cell_item_name(cell: Vector3) -> String:
+	var cell_item_index = grid_map.get_cell_item(cell.x, cell.y, cell.z)
+	var cell_item_name = grid_map.mesh_library.get_item_name(cell_item_index)
+	return cell_item_name
+
+func v3_to_index(v3) -> String:
+	return str(int(round(v3.x))) + "," + str(int(round(v3.y))) + "," + str(int(round(v3.z)))
+	
+func get_gm_path(start, end) -> PoolVector3Array:
+	print_debug(start, end)
+	var gm_start = v3_to_index(grid_map.world_to_map(start))
+	var gm_end = v3_to_index(grid_map.world_to_map(end))
+	var start_id = 0
+	var end_id = 0
+	if gm_start in all_points:
+		start_id = all_points[gm_start]
+	else:
+		start_id = as_node.get_closest_point(start)
+	if gm_end in all_points:
+		end_id = all_points[gm_end]
+	else:
+		end_id = as_node.get_closest_point(end)
+	return as_node.get_point_path(start_id, end_id)
