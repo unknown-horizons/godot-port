5e5942521d71bc43d8c0127d358e9d871691d944
Aaron Franke <arnfranke@yahoo.com>
2020-03-08 02:32:00 -0400

    Some minor code cleanup

--- Assets/World/AStarMap.gd
+++ Assets/World/AStarMap.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 	ray_plane.mesh = PlaneMesh.new()
 	ray_plane.visible = false
 
-	ray_plane.scale = Vector3(128, 128, 128) # TODO: Adapt size dynamically to GridMap's outer bounds
+	ray_plane.scale = Vector3.ONE * 128 # TODO: Adapt size dynamically to GridMap's outer bounds
 
 	#ray_plane.create_convex_collision() # crashes for GLES2 in the export build
 
@@ -25,6 +25,7 @@ func _ready() -> void:
 	add_child(ray_plane)
 	
 	as_node = AStar.new()
+	# TODO: Make use of Vector3i in Godot 4.0.
 	var cells = grid_map.get_used_cells()
 	for cell in cells:
 		if _get_cell_item_name(cell) in ["deep", "shallow_curve_in"]:
@@ -54,8 +55,10 @@ func _get_cell_item_name(cell: Vector3) -> String:
 	return cell_item_name
 
 func v3_to_index(v3: Vector3) -> String:
-	return str(int(round(v3.x))) + "," + str(int(round(v3.y))) + "," + str(int(round(v3.z)))
-	
+	# TODO: Make use of Vector3i in Godot 4.0.
+	v3 = v3.round()
+	return str(int(v3.x)) + "," + str(int(v3.y)) + "," + str(int(v3.z))
+
 func get_gm_path(start: Vector3, end: Vector3) -> PoolVector3Array:
 	#print_debug(start, end)
 	var gm_start = v3_to_index(grid_map.world_to_map(start))
