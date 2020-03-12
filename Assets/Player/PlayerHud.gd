62c407d987aed80675ca01c35da6093153638485
Raphael Roy <raphael.roy@uni-bielefeld.de>
2020-03-12 12:19:44 +0100

    Forgot to commit PlayerHud.gd

--- Assets/Player/PlayerHud.gd
+++ Assets/Player/PlayerHud.gd
@@ -17,6 +17,24 @@ var _debug_messages = [
 
 func _process(_delta):
 	pass
+	
+func _input(event: InputEvent) -> void:
+	_on_input(event)
+	
+func _on_input(event):
+	var selected_units = get_tree().get_nodes_in_group("selected_units")
+	if selected_units.size() == 0:
+		# should in the future hide all unit-related HUD-Elements
+		$MarginContainer/HBoxContainer/ShipMenuTabWidget.visible = false
+	elif selected_units.size() == 1: 
+		# only one Unit, so probably only possible to build etc. if only one Unit selected
+		if selected_units[0].get_groups().find("ships") == 1: # Its a ship
+			$MarginContainer/HBoxContainer/ShipMenuTabWidget.visible = true
+		if selected_units[0].get_groups().find("ships") != 1: # Its not a ship. Do something else
+			pass
+	elif selected_units.size() > 1: # multiple units selected, do something else
+		pass
+	
 
 func raise_notification(message_type, message_text):
 	var _debug_message = _debug_messages[randi() % _debug_messages.size()]
