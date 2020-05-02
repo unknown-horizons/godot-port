93d7d82fc4aa27ad423291542cada1f1c06d5fa2
Burkhard <burkhard.ramme@gmail.com>
2020-05-02 07:16:56 +0200

    fix style

--- Assets/UI/Scripts/OptionsButton.gd
+++ Assets/UI/Scripts/OptionsButton.gd
@@ -1,8 +1,7 @@
 extends Button
 class_name OptionsButton
 signal button(option_name)
-	
+
 func _on_Button_pressed(option_name) -> void:
 	Audio.play_snd_click()
 	emit_signal("button", option_name)
-	
