6c7b3d54d66ddad34e19ed1965583687d1984c26
Arthur S. Muszynski <artism90@googlemail.com>
2022-03-22 19:46:00 +0100

    Fix faction color overlapping

--- Assets/World/Units/Ships/Merchants/Huker/Huker.gd
+++ Assets/World/Units/Ships/Merchants/Huker/Huker.gd
@@ -2,19 +2,11 @@ tool
 extends Ship
 class_name Huker
 
-onready var faction_color: Sprite3D = $FactionColor as Sprite3D
+onready var faction_color: Sprite3D = find_node("FactionColor") as Sprite3D
 
 func update_faction_color() -> void:
 	if faction_color != null:
-
-		# Use a preconfigured material as a base
-		var material = Global.COLOR_MATERIAL[faction] as ShaderMaterial
-		material = material.duplicate()
-
-		# Apply specific parameters to this instance
-		material.set_shader_param("texture_albedo", texture)
-
-		faction_color.set_material_override(material)
+		faction_color.modulate = Global.FACTION_COLORS[faction]
 
 		# Match rotation of the ship's color outline with the main texture rotation
 		faction_color.frame = _billboard.frame
