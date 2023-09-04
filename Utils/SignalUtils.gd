7482cf7834ed034805cfb15a46501e2f79afdb8d
IcyLeave6109 <33421921+juliohq@users.noreply.github.com>
2023-09-04 06:37:51 -0300

    Remove another redundant line
    Co-authored-by: Arthur S. Muszynski <artism90@googlemail.com>

--- Utils/SignalUtils.gd
+++ Utils/SignalUtils.gd
@@ -6,7 +6,6 @@ static func ensure_connected(_signal: Signal, callable: Callable):
 	if not _signal.is_connected(callable):
 		_signal.connect(callable)
 
-
 ## Makes sure the given signal is not connected to the given callable and disconnects it otherwise.
 static func ensure_disconnected(_signal: Signal, callable: Callable):
 	assert(callable.is_valid())
