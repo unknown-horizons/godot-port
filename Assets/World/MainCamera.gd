b03e7439925e0834b4c0c2e49d5a40c358d4d6f4
MichaelXt <MichaelXt@users.noreply.github.com>
2025-01-22 12:11:18 -0800

    zoom fix (#19)
    "windowed" main windo

--- Assets/World/MainCamera.gd
+++ Assets/World/MainCamera.gd
@@ -1,37 +1,25 @@
 extends Camera2D
 
-@export var move_sensitivity: float = 5
-@export var zoom_sensitivity: float = 1
+@export var zoom_sensitivity: float = 0.5
+@export var zoom_duration := 0.2
 
 var should_move_camera = false
 
-
+func set_zoom_smooth(new_zoom: float):
+  var tween = self.get_tree().create_tween()
+  tween.tween_property(self, "zoom", Vector2(new_zoom, new_zoom), zoom_duration) \
+    .set_trans(Tween.TRANS_SINE) \
+    .set_ease(Tween.EASE_OUT)
 
 func _unhandled_input(event):
-  check_and_move_camera(event)
-  check_and_zoom_camera(event)
+  if event is InputEventMouseMotion:
+    if Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
+      self.position += -event.relative / self.zoom.x
 
-
-
-func check_and_move_camera(event):
-  if event is InputEventMouseButton:
-    if event.pressed and event.button_index == MOUSE_BUTTON_MIDDLE:
-      should_move_camera = true
-  
-    if not event.pressed:
-      should_move_camera = false
-
-  if event is InputEventMouseMotion and should_move_camera:
-    self.position += - event.screen_relative * move_sensitivity
-
-
-
-func check_and_zoom_camera(event):
-  if event is InputEventMouseButton:
-    if event.pressed == true:
-
-      if event.button_index == MOUSE_BUTTON_WHEEL_UP and self.zoom < Vector2(2, 2):
-        self.zoom += self.zoom * Vector2(zoom_sensitivity, zoom_sensitivity)
-
-      if event.button_index == MOUSE_BUTTON_WHEEL_DOWN and self.zoom > Vector2(0.2, 0.2):
-        self.zoom -= self.zoom / Vector2(zoom_sensitivity, zoom_sensitivity) / 2
+  if event is InputEventMouseButton and event.pressed:
+    if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+      if self.zoom.x < 2:
+        set_zoom_smooth(self.zoom.x +  zoom_sensitivity * self.zoom.x)
+    if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+      if self.zoom.x > 0.2:
+        set_zoom_smooth(self.zoom.x -  zoom_sensitivity * self.zoom.x)
